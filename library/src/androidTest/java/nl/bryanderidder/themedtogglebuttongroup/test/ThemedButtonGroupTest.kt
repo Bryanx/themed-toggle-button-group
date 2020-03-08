8b36495f58ef1726648cbb30a7b797b842a89956
Bryan de Ridder <br.deridder@gmail.com>
2020-03-08 12:46:34 +0100

    Added multiple selection test

--- library/src/androidTest/java/nl/bryanderidder/themedtogglebuttongroup/test/ThemedButtonGroupTest.kt
+++ library/src/androidTest/java/nl/bryanderidder/themedtogglebuttongroup/test/ThemedButtonGroupTest.kt
@@ -1,12 +1,17 @@
 package nl.bryanderidder.themedtogglebuttongroup.test
 
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.core.animation.doOnEnd
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.FlakyTest
 import androidx.test.filters.MediumTest
 import androidx.test.rule.ActivityTestRule
 import junit.framework.Assert.assertNotNull
 import junit.framework.Assert.assertTrue
+import nl.bryanderidder.themedtogglebuttongroup.ThemedButton
 import nl.bryanderidder.themedtogglebuttongroup.ThemedButtonGroup
+import nl.bryanderidder.themedtogglebuttongroup.name
 import org.hamcrest.Matchers.*
 import org.junit.Assert.assertThat
 import org.junit.Rule
@@ -26,20 +31,59 @@ class ThemedButtonGroupTest {
     @Throws(Throwable::class)
     fun testLoadFromLayoutXml() {
         val buttonGroup = createLayout(R.layout.activity_simple, activityRule)
+        val button1 = buttonGroup.findViewById<ThemedButton>(R.id.btn1)
         assertNotNull(buttonGroup)
         assertThat(buttonGroup, isA(ThemedButtonGroup::class.java))
         assertThat(buttonGroup.childCount, `is`(3))
-        assertThat(buttonGroup.buttons, hasSize(3))
-        assertThat(buttonGroup.buttons[0].text, `is`("5:30PM"))
-        assertTrue(buttonGroup.buttons.all { it.btnBackgroundColor == it.defaultBgColor })
-        assertTrue(buttonGroup.buttons.all { it.textColor == it.defaultTextColor })
-        assertTrue(buttonGroup.buttons.all { !it.isSelected })
+        assertThat(button1.text, `is`("5:30PM"))
+        val buttons = buttonGroup.buttons
+        assertThat(buttons, hasSize(3))
+        assertTrue(buttons.all { it.btnBackgroundColor == it.defaultBgColor })
+        assertTrue(buttons.all { it.textColor == it.defaultTextColor })
+        assertTrue(buttons.all { !it.isSelected })
+        assertTrue(buttons.all { it.cbCardView.visibility == VISIBLE })
+        assertTrue(buttons.all { it.cbCardViewHighlight.visibility == GONE })
+        //select a single button
+        buttonGroup.selectButton(buttons[0], 0F, 0F, true).doOnEnd {
+            assertThat(buttons.single { it.isSelected }.name, `is`(buttons[0].name))
+        }
+        //select another button
+        buttonGroup.selectButton(buttons[1], 0F, 0F, true).doOnEnd {
+            assertThat(buttons.single { it.isSelected }.name, `is`(buttons[1].name))
+        }
+        //select last button
+        buttonGroup.selectButton(buttons[2], 0F, 0F, true).doOnEnd {
+            assertThat(buttons.single { it.isSelected }.name, `is`(buttons[2].name))
+        }
+    }
 
-        buttonGroup.selectButton(buttonGroup.buttons[0], 0F, 0F, true)
-
-        assertTrue(buttonGroup.buttons[0].isSelected)
-        assertTrue(!buttonGroup.buttons[1].isSelected)
-        assertTrue(!buttonGroup.buttons[2].isSelected)
+    @Test
+    @FlakyTest
+    @Throws(Throwable::class)
+    fun testMultipleSelection() {
+        val buttonGroup = createLayout(R.layout.activity_multiple_selection, activityRule)
+        val buttons = buttonGroup.buttons
+        //select 1 button
+        buttonGroup.selectButton(buttons[0], 0F, 0F, true).doOnEnd {
+            assertThat(buttons.single { it.isSelected }.name, `is`(buttons[0].name))
+            assertThat(buttons[0].cbCardViewHighlight.visibility, `is`(VISIBLE))
+            assertThat(buttons[1].cbCardViewHighlight.visibility, `is`(GONE))
+            assertThat(buttons[2].cbCardViewHighlight.visibility, `is`(GONE))
+        }
+        //select another button
+        buttonGroup.selectButton(buttons[1], 0F, 0F, true).doOnEnd {
+            assertThat(buttons.filter { it.isSelected }, hasItems(buttons[0], buttons[1]))
+            assertThat(buttons[0].cbCardViewHighlight.visibility, `is`(VISIBLE))
+            assertThat(buttons[1].cbCardViewHighlight.visibility, `is`(VISIBLE))
+            assertThat(buttons[2].cbCardViewHighlight.visibility, `is`(GONE))
+        }
+        //select last button
+        buttonGroup.selectButton(buttons[2], 0F, 0F, true).doOnEnd {
+            assertThat(buttons.filter { it.isSelected }, hasItems(buttons[1], buttons[2]))
+            assertThat(buttons[0].cbCardViewHighlight.visibility, `is`(GONE))
+            assertThat(buttons[1].cbCardViewHighlight.visibility, `is`(VISIBLE))
+            assertThat(buttons[2].cbCardViewHighlight.visibility, `is`(VISIBLE))
+        }
     }
 
 }
