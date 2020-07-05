0cca04ce2c7bd8e27d630bbd9bcc3cb7e215d856
Bryan de Ridder <br.deridder@gmail.com>
2020-07-05 15:07:17 +0200

    Update MainActivity.kt

--- demo-programmatically-add-buttons/src/main/java/nl/bryanderidder/demo_programmatically_add_buttons/MainActivity.kt
+++ demo-programmatically-add-buttons/src/main/java/nl/bryanderidder/demo_programmatically_add_buttons/MainActivity.kt
@@ -16,18 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Create the toggle group and add it to the rootView
         val buttonGroup = ThemedToggleButtonGroup(this)
-        buttonGroup.justifyContent = JustifyContent.CENTER
+        buttonGroup.justifyContent = JustifyContent.CENTER // this is optional
         rootView.addView(buttonGroup)
 
+        // Create the 1st button and add it to the toggle group
         val btn1 = ThemedButton(buttonGroup.context)
         btn1.text = "Button 1"
         buttonGroup.addView(btn1, ViewGroup.LayoutParams(WRAP_CONTENT, MATCH_PARENT))
 
+        // Create the 2nd button and add it to the toggle group
         val btn2 = ThemedButton(buttonGroup.context)
         btn2.text = "Button 2"
         buttonGroup.addView(btn2, ViewGroup.LayoutParams(WRAP_CONTENT, MATCH_PARENT))
 
+        // Create the 3rd button and add it to the toggle group
         val btn3 = ThemedButton(buttonGroup.context)
         btn3.text = "Button 3"
         buttonGroup.addView(btn3, ViewGroup.LayoutParams(WRAP_CONTENT, MATCH_PARENT))
