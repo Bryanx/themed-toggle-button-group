2bde783fb935e2342a63b3b9c0b6a4b9c2cd0bd9
Bryan de Ridder <br.deridder@gmail.com>
2021-02-07 22:03:59 +0100

    Update copyright

--- library/src/androidTest/java/nl/bryanderidder/themedtogglebuttongroup/test/ThemedToggleButtonGroupTest.kt
+++ library/src/androidTest/java/nl/bryanderidder/themedtogglebuttongroup/test/ThemedToggleButtonGroupTest.kt
@@ -3,7 +3,7 @@
  * ThemedToggleButtonGroup
  *
  * Created by Bryan de Ridder on 15/3/20.
- * Copyright (c) 2020 Bryan de Ridder (br.deridder@gmail.com)
+ * Copyright (c) 2021 Bryan de Ridder (br.deridder@gmail.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
